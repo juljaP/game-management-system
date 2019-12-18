532114f5a7fd3815ece8f3d35c7b0ce3cf6e7223
juljasong <juljasong@gmail.com>
2019-12-18 18:04:23 +0900

    v004

--- src/main/java/julja/gms/App.java
+++ src/main/java/julja/gms/App.java
@@ -1,9 +1,23 @@
-// 프로젝트: 게임 타이틀 관리 시스템
 package julja.gms;
 
 public class App {
-  
-    public static void main(String[] args) {
-       System.out.println("공략한 캐 다시 공략하지 말자");
-    }
+  public static void main(String[] args) {
+    
+    int gameNum = 1;
+    String gameName = "A_A";
+    String gameProduction = "B_B";
+    String gamePlatform = "C_C";
+    String gameGenre = "D_D";
+    String gameIllust = "E_E";
+    Boolean gameVoice = true;
+    
+    System.out.println("게임번호 : " + gameNum);
+    System.out.println("게임명 : " + gameName);
+    System.out.println("제작사 : " + gameProduction);
+    System.out.println("플랫폼 : " + gamePlatform);
+    System.out.println("장르 : " + gameGenre);
+    System.out.println("작화 : " + gameIllust);
+    System.out.println("음성 : " + gameVoice);
+    
+  }
 }
