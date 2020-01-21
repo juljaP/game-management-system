4b6f7563acf14494969c688a583454ada87ea5c5
juljasong <juljasong@gmail.com>
2020-01-21 18:31:20 +0900

    v026 : Command Pattern

--- README.md
+++ README.md
@@ -1,31 +1,48 @@
-# 25 - 자바 컬렉션 API 사용하기
+# 26 - `커맨드(Command)` 디자인 패턴을 적용
 
-- 자바에서 제공하는 자료구조 구현체 활용
+##  결과
 
-## 결과
-
-- src/main/java/julja/util/List.java 삭제
-- src/main/java/julja/util/AbstractList.java 삭제
-- src/main/java/julja/util/ArrayList.java 삭제
-- src/main/java/julja/util/LinkedList.java 삭제
-- src/main/java/julja/util/Iterator.java 삭제
-- src/main/java/julja/util/Stack.java 삭제
-- src/main/java/julja/util/Queue.java 삭제
-- src/main/java/julja/util 삭제
-- src/main/java/julja/gms/handler/LessonHandler.java 변경
-- src/main/java/julja/gms/handler/MemberHandler.java 변경
-- src/main/java/julja/gms/handler/BoardHandler.java 변경
+- src/main/java/julja/gms/handler/Command.java 추가
+- src/main/java/julja/gms/handler/GameAddCommand.java 추가
+- src/main/java/julja/gms/handler/GameListCommand.java 추가
+- src/main/java/julja/gms/handler/GameDetailCommand.java 추가
+- src/main/java/julja/gms/handler/GameUpdateCommand.java 추가
+- src/main/java/julja/gms/handler/GameDeleteCommand.java 추가
+- src/main/java/julja/gms/handler/UserAddCommand.java 추가
+- src/main/java/julja/gms/handler/UserListCommand.java 추가
+- src/main/java/julja/gms/handler/UserDetailCommand.java 추가
+- src/main/java/julja/gms/handler/UserUpdateCommand.java 추가
+- src/main/java/julja/gms/handler/UserDeleteCommand.java 추가
+- src/main/java/julja/gms/handler/BoardAddCommand.java 추가
+- src/main/java/julja/gms/handler/BoardListCommand.java 추가
+- src/main/java/julja/gms/handler/BoardDetailCommand.java 추가
+- src/main/java/julja/gms/handler/BoardUpdateCommand.java 추가
+- src/main/java/julja/gms/handler/BoardDeleteCommand.java 추가
+- src/main/java/julja/gms/handler/GameHandler.java 삭제
+- src/main/java/julja/gms/handler/UserHandler.java 삭제
+- src/main/java/julja/gms/handler/BoardHandler.java 삭제
 - src/main/java/julja/gms/App.java 변경
 
 
-### 1. ArrayList, LinkedList, Stack, Queue 클래스를 자바 컬렉션 API로 교체하라.
+### 1. 메서드를 호출하는 쪽과 실행 쪽 사이의 규칙을 정의하라.
 
-- LessonHandler.java
-    - `List` 를 `java.util.List` 인터페이스로 교체한다.
-- MemberHandler.java
-    - `List` 를 `java.util.List` 인터페이스로 교체한다.
+- Command.java
+    - `App` 클래스와 명령을 처리하는 클래스 사이의 호출 규칙을 정의한다.
+
+### 2. 명령을 처리하는 각 메서드를 객체로 분리하라.
+
+- GameHandler.java
+    - 수업 CRUD 각 기능을 `Command` 규칙에 따라 객체로 분리한다.
+- UserHandler.java
+    - 수업 CRUD 각 기능을 `Command` 규칙에 따라 객체로 분리한다.
 - BoardHandler.java
-    - `List` 를 `java.util.List` 인터페이스로 교체한다.
+    - 수업 CRUD 각 기능을 `Command` 규칙에 따라 객체로 분리한다.
+- App.java (App.java.01)
+    - 명령어가 입력되면 `Command` 규칙에 따라 객체를 실행한다.
+    - `/board2/xxx` 명령 처리는 삭제한다.
+
+### 3: `Map`으로 `Command` 객체를 관리하라.
+
 - App.java
-    - 핸들러를 생성할 때 자바 컬렉션 API에서 제공하는 `java.util.List`의 구현체를 넘겨준다.
-    - 명령 내역을 저장할 때도 자바 컬렉션 API의 Stack과 Queue 구현체를 사용한다.
+    - 명령어를 `key`, `Command` 객체를 `value`로 하여 Map에 저장한다.
+    - 각 명령에 대해 조건문으로 분기하는 부분을 간략하게 변경한다.
--- src/main/java/julja/gms/App.java
+++ src/main/java/julja/gms/App.java
@@ -3,13 +3,27 @@ package julja.gms;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Deque;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
-import julja.gms.Handler.BoardHandler;
-import julja.gms.Handler.GameHandler;
-import julja.gms.Handler.UserHandler;
+import julja.gms.Handler.BoardAddCommand;
+import julja.gms.Handler.BoardDeleteCommand;
+import julja.gms.Handler.BoardDetailCommand;
+import julja.gms.Handler.BoardListCommand;
+import julja.gms.Handler.BoardUpdateCommand;
+import julja.gms.Handler.Command;
+import julja.gms.Handler.GameAddCommand;
+import julja.gms.Handler.GameDeleteCommand;
+import julja.gms.Handler.GameDetailCommand;
+import julja.gms.Handler.GameListCommand;
+import julja.gms.Handler.GameUpdateCommand;
+import julja.gms.Handler.UserAddCommand;
+import julja.gms.Handler.UserDeleteCommand;
+import julja.gms.Handler.UserDetailCommand;
+import julja.gms.Handler.UserListCommand;
+import julja.gms.Handler.UserUpdateCommand;
 import julja.gms.domain.Board;
 import julja.gms.domain.Game;
 import julja.gms.domain.User;
@@ -24,87 +38,60 @@ public class App {
 
   public static void main(String[] args) {
 
-    Prompt prompt = new Prompt(sc);
-    
+    Prompt prompt = new Prompt(App.sc);
+    HashMap<String, Command> commandMap = new HashMap<>();
+
     ArrayList<Game> gameList = new ArrayList<>();
     ArrayList<Board> boardList = new ArrayList<>();
     LinkedList<User> userList = new LinkedList<>();
 
-    GameHandler gh = new GameHandler(prompt, gameList);
-    UserHandler uh = new UserHandler(prompt, userList);
-    BoardHandler bh = new BoardHandler(prompt, boardList);
+    commandMap.put("/board/add", new BoardAddCommand(prompt, boardList));
+    commandMap.put("/board/delete", new BoardDeleteCommand(prompt, boardList));
+    commandMap.put("/board/detail", new BoardDetailCommand(prompt, boardList));
+    commandMap.put("/board/list", new BoardListCommand(boardList));
+    commandMap.put("/board/update", new BoardUpdateCommand(prompt, boardList));
+
+    commandMap.put("/game/add", new GameAddCommand(prompt, gameList));
+    commandMap.put("/game/delete", new GameDeleteCommand(prompt, gameList));
+    commandMap.put("/game/detail", new GameDetailCommand(prompt, gameList));
+    commandMap.put("/game/list", new GameListCommand(gameList));
+    commandMap.put("/game/update", new GameUpdateCommand(prompt, gameList));
+
+    commandMap.put("/user/add", new UserAddCommand(prompt, userList));
+    commandMap.put("/user/delete", new UserDeleteCommand(prompt, userList));
+    commandMap.put("/user/detail", new UserDetailCommand(prompt, userList));
+    commandMap.put("/user/list", new UserListCommand(userList));
+    commandMap.put("/user/update", new UserUpdateCommand(prompt, userList));
+
 
     while (true) {
       String command = prompt();
 
-      if(command.length() == 0) {
+      if (command.length() == 0) {
+        continue;
+      } else if (command.equalsIgnoreCase("quit")) {
+        System.out.println("안녕!");
+        break;
+      } else if (command.equalsIgnoreCase("history")) {
+        printCommandHistory(App.stack.iterator());
+        System.out.println();
+        continue;
+      } else if (command.equalsIgnoreCase("history2")) {
+        printCommandHistory(App.queue.iterator());
+        System.out.println();
         continue;
       }
 
-      stack.push(command);
-      queue.offer(command);
+      App.stack.push(command);
+      App.queue.offer(command);
 
-      switch (command) {
-        case "/game/add" :
-          gh.addGame();
-          break;
-        case "/game/list" :
-          gh.listGame();
-          break;
-        case "/game/detail" :
-          gh.detailGame();
-          break;
-        case "/game/update" :
-          gh.updateGame();
-          break;
-        case "/game/delete" :
-          gh.deleteGame();
-          break;
-        case "/user/add" :
-          uh.addUser();
-          break;
-        case "/user/list" :
-          uh.listUser();
-          break;
-        case "/user/detail" :
-          uh.detailUser();
-          break;
-        case "/user/update" :
-          uh.updateUser();
-          break;
-        case "/user/delete" :
-          uh.deleteUser();
-          break;
-        case "/board/add" :
-          bh.addBoard();
-          break;
-        case "/board/list" :
-          bh.listBoard();
-          break;
-        case "/board/detail" :
-          bh.detailBoard();
-          break;
-        case "/board/update" :
-          bh.updateBoard();
-          break;
-        case "/board/delete" :
-          bh.deleteBoard();
-          break;
-        case "history" :
-          printCommandHistory(stack.iterator());
-          break;
-        case "history2" :
-          printCommandHistory(queue.iterator());
-          break;
-        case "quit" : 
-          sc.close();
-          System.out.println("안녕!");
-          System.exit(0);
-        default :
-          System.out.println("실행할 수 없는 명령입니다.");
-          System.out.println();
-          break;
-      }
+      Command commandHandler = commandMap.get(command);
+
+      if (commandHandler != null)
+        commandHandler.execute();
+      else
+        System.out.println("실행할 수 없는 명령입니다.");
+      System.out.println();
     }
   }
 
@@ -113,22 +100,22 @@ public class App {
     Iterator<String> iterator = iter;
     int count = 0;
     String answer = null;
-    while(iterator.hasNext()) {
+    while (iterator.hasNext()) {
       System.out.println(iterator.next());
-      if(++count % 5 == 0) {
+      if (++count % 5 == 0) {
         System.out.print(": ");
-        answer = sc.nextLine();
-        if(answer.equalsIgnoreCase("q")) {
+        answer = App.sc.nextLine();
+        if (answer.equalsIgnoreCase("q")) {
           break;
         }
       }
     }
     System.out.println();
   }
-  
+
   private static String prompt() {
     System.out.print("명령> ");
-    String command = sc.nextLine();
+    String command = App.sc.nextLine();
     return command;
   }
 }
